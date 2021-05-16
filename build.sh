db5bf8cb0ca976cfaa90e9b0a3473a5998c90549
lifegpc <g1710431395@gmail.com>
2021-05-16 14:36:03 +0800

    upload

---
+++ build.sh
@@ -0,0 +1,5 @@
+cd packages || exit 1
+git apply --ignore-space-change --ignore-whitespace '../mingw-w64-libxml2.patch' || exit 1
+cd mingw-w64-harfbuzz || exit 1
+dos2unix PKGBUILD || exit 1
+MINGW_ARCH=mingw64 makepkg-mingw -sLf --nosign --noconfirm --skippgpcheck --noprogressbar --skipchecksums || exit 1
