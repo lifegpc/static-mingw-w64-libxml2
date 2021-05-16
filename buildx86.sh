b493d4b944f1fbd9037b3386f5040eca5b617a65
lifegpc <g1710431395@gmail.com>
2021-05-17 07:15:21 +0800

    fix wrong name

--- buildx86.sh
+++ buildx86.sh
@@ -1,5 +1,5 @@
 cd packages || exit 1
 git apply --ignore-space-change --ignore-whitespace '../mingw-w64-libxml2.patch' || exit 1
-cd mingw-w64-harfbuzz || exit 1
+cd mingw-w64-libxml2 || exit 1
 dos2unix PKGBUILD || exit 1
 MINGW_ARCH=mingw32 makepkg-mingw -sLf --nosign --noconfirm --skippgpcheck --noprogressbar --skipchecksums || exit 1
